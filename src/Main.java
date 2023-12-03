ee52dfc1799eefff87bf9140244b77fea3b0c467
Chris McCambridge <chrissiemcc44@gmail.com>
2023-12-03 20:33:07 +0000

    Added error handling

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,78 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     static final double pi = 3.14159265359;
     static Scanner myObj = new Scanner(System.in);
+    static boolean correctInput;
 
     public static void main(String[] args) {
-        int input = 1;
+        int input = -1;
+
         while(input != 0){
+            input = -1; //reset the input for each iteration of the while loop
+            correctInput = false;
+
             System.out.println("""
                 What shape do you want to calculate the volume of?
                 1) Cube
                 2) Cylinder
                 3) Sphere
                 0) Exit Program""");
-            input = myObj.nextInt(); //get user input for the shape they want
+
+            try{
+                input = myObj.nextInt(); //get user input for the shape they want
+            }catch (InputMismatchException ime){
+                System.out.println("This input is invalid");
+                myObj.next(); //progress scanner obj from invalid input
+            }
 
             switch (input) {
+                case 0 -> {}
                 case 1 -> cubeVolume(getLength());
                 case 2 -> cylinderVolume(getRadius(), getLength());
                 case 3 -> sphereVolume(getRadius());
-                default -> {}
+                default -> {
+                    System.out.println("Please input a number from the menu");
+                }
             }//call methods based on the user input
         }
     }
 
     private static double getLength() {
-        System.out.println("What is the length?");
-        return myObj.nextDouble();
+        double length = 0.0;
+
+        while (!correctInput) {
+            try{
+                System.out.println("What is the length?");
+                correctInput = true;
+                length = myObj.nextDouble();
+            }catch (InputMismatchException ime){
+                correctInput = false;
+                myObj.next();
+                System.out.println("This input is invalid");
+            }
+        }
+
+        return length;
     }//get user input for the length of the shape
 
     private static double getRadius() {
-        System.out.println("What is the radius?");
-        return myObj.nextDouble();
+        double radius = 0.0;
+
+        while (!correctInput) {
+            try{
+                System.out.println("What is the radius?");
+                correctInput = true;
+                radius = myObj.nextDouble();
+            }catch (InputMismatchException ime){
+                correctInput = false;
+                myObj.next();
+                System.out.println("This input is invalid");
+            }
+        }
+
+        return radius;
     }//get user input for the radius of the shape
 
     private static void sphereVolume(double radius) {
