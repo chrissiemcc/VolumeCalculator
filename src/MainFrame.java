dc69e0c38c7d221f4c82429776ed6a4d11408cfe
Chris McCambridge <chrissiemcc44@gmail.com>
2024-02-19 17:44:46 +0000

    Started to populate panel dynamically

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -16,7 +16,7 @@ public class MainFrame extends JFrame {
         toolbar.setStringListener(new StringListener() {
             @Override
             public void textEmitted(String text) {
-                textPanel.appendText(text);
+                if(text.equals("Cube")) textPanel.setCubePanel();
             }
         });
 
--- src/TextPanel.java
+++ src/TextPanel.java
@@ -1,20 +1,42 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class TextPanel extends JPanel {
-    private JTextArea textArea;
-    private JLabel lblSelectedShape;
+    private final JTextField txtLength = new JTextField(10);
+    private final JLabel lblLength = new JLabel("Length:");
+    GridBagConstraints gc = new GridBagConstraints();
 
     public TextPanel() {
-        //lblSelectedShape = new JLabel("Default");
+        Border innerBorder = BorderFactory.createTitledBorder("Volume Calculator");
+        Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
 
-        textArea = new JTextArea();
-
-        setLayout(new BorderLayout());
-        add(textArea);
+        setLayout(new GridBagLayout());
     }
 
-    public void appendText(String text) {
-        textArea.append(text);
+    public void setCubePanel() {
+        clearPanel();
+
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+
+        gc.gridx = 0;
+        gc.gridy = 0;
+        add(lblLength, gc);
+
+        gc.gridx = 1;
+        add(txtLength, gc);
+
+        refreshPanel();
+    }
+
+    public void clearPanel() {
+        this.removeAll();
+    }
+
+    public void refreshPanel() {
+        this.revalidate();
     }
 }
