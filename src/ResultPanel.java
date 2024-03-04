68d9784ca485a6bcb296fb239046cb928e9a3707
Chris McCambridge <chrissiemcc44@gmail.com>
2024-03-04 19:56:23 +0000

    Reformat code for the result panel

--- src/ResultPanel.java
+++ src/ResultPanel.java
@@ -3,9 +3,9 @@ import java.awt.*;
 
 public class ResultPanel extends JPanel {
     public ResultPanel() {
-        setLayout(new FlowLayout());
-
         JButton btnCalculate = new JButton("Calculate");
         add(btnCalculate);
+
+        setLayout(new FlowLayout());
     }
 }
