e99fea740e3c19e44fee03d78a01d3d11a77c521
Chris McCambridge <chrissiemcc44@gmail.com>
2024-04-15 17:24:16 +0100

    Started to add calculation result design

--- src/ResultPanel.java
+++ src/ResultPanel.java
@@ -4,6 +4,9 @@ import java.awt.*;
 public class ResultPanel extends JPanel {
     public ResultPanel() {
         JButton btnCalculate = new JButton("Calculate");
+        JLabel lblResult = new JLabel("Result");
+
+        add(lblResult);
         add(btnCalculate);
 
         setLayout(new FlowLayout());
